8347c09f48dafbc514d5a1e7f8949b4f57269d6b
BrunoQuenan98 <brunoquenan@gmail.com>
2022-04-13 20:34:47 -0300

    se instalo react-router, react-router-dom, se realizo el ruteo en App.js y se crearon 5 componentes de prueba

--- src/App.js
+++ src/App.js
@@ -1,56 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import { Route, Routes } from 'react-router';
+import { Home } from './components/home';
+import { Activities } from './components/activities';
+import { Contributes } from './components/contributes';
+import { OngInformation } from './components/ongInformation';
+import { NavBar } from './components/navBar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+      <NavBar />
+      <Routes>
+      <Route to exact path='/' element={<Home/>}/>
+      <Route to path='/nosotros' element={<OngInformation/>}/>
+      <Route to path='/actividades' element={<Activities/>}/>
+      <Route to path='/contribuye' element={<Contributes/>}/>
+      </Routes>
     </div>
   );
 }
