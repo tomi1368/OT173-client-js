93482bb7fe74d9c9ed6f6931b3e3604d95c898b2
Tomas Rodriguez <tomirodriguez1368@gmail.com>
2022-04-14 01:51:07 -0300

    updated: axios login setup

--- src/components/Login/loginFormSettings/loginRequest.js
+++ src/components/Login/loginFormSettings/loginRequest.js
@@ -1,3 +1,16 @@
-export const loginRequest = ()=>{
-    
+import axios from "axios"
+export const loginRequest = async (formValue,navigate)=>{
+
+    try{
+        /* navigate una vez hago login voy a inicio */
+        /* Falta link de api */
+        const loginResponse = await axios.post("",formValue)
+    }catch(err){
+        /* Manejar estado de error */
+        console.log(err)
+    }
+
+
+
+
 }
