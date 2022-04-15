12153677565438dcad438331ad2c70771b4e782a
Tomas Rodriguez <tomirodriguez1368@gmail.com>
2022-04-14 23:37:30 -0300

    updated: navbar style and structure

--- src/components/Home/presentational/NavBar/NavBar.jsx
+++ src/components/Home/presentational/NavBar/NavBar.jsx
@@ -3,22 +3,22 @@ import "./NavBar.scss";
 const NavBar = () => {
   return (
     <nav className="nav">
-      <div className="container">
-        <div className="">
-          {/* <img src="/images/404.png" alt="" /> */}
+      <div className="nav__container">
+        <div className="nav__container--logo">
+          <img src="/images/404.png" alt="" />
         </div>
-        <ul>
-          <li>Inicio</li>
-          <li>Nosotros</li>
-          <li>Actividades</li>
-          <li>Novedades</li>
-          <li>Testimonios</li>
-          <li>Contacto</li>
-          <li>Contribuye</li>
+        <ul className="nav__container--links">
+          <li className="nav__container--links--link">Inicio</li>
+          <li className="nav__container--links--link">Nosotros</li>
+          <li className="nav__container--links--link">Actividades</li>
+          <li className="nav__container--links--link">Novedades</li>
+          <li className="nav__container--links--link">Testimonios</li>
+          <li className="nav__container--links--link">Contacto</li>
+          <li className="nav__container--links--link">Contribuye</li>
         </ul>
-        <div>
-          <button>Log In</button>
-          <button>Sign Up</button>
+        <div className="nav__container--btns">
+          <button className="nav__container--btns--btn login-btn">Log In</button>
+          <button className="nav__container--btns--btn sign-btn">Sign Up</button>
         </div>
       </div>
     </nav>
