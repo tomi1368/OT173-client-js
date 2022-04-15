95480f3e645998624ca2802bb81ebe80ab1faf7d
Tomas Rodriguez <tomirodriguez1368@gmail.com>
2022-04-15 12:59:04 -0300

    updated: Show User Hello

---
+++ src/components/Home/presentational/HelloUser/HelloUser.jsx
@@ -0,0 +1,8 @@
+import "./HelloUser.scss"
+const HelloUser = () => {
+  return (
+    <h1 className="hello">Hello Roberto Diaz! Welcome Back! <span className="hello-hand">&#128400;</span></h1>
+  )
+}
+
+export default HelloUser
