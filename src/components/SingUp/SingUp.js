b24c8f5fc14d0a3e6e2706b13feb7ac6b1dc968d
Nicolás Cabello <cabellonic@gmail.com>
2022-04-14 23:26:25 -0300

    update: Add error messages to the Sing Up form

--- src/components/SingUp/SingUp.js
+++ src/components/SingUp/SingUp.js
@@ -1,18 +1,24 @@
-import { Formik, Form, Field } from "formik";
+import { useState } from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 
 import {
 	SingUpFormSchema,
+	__ERROR_MESSAGE__,
 	__INITIAL_VALUES_SINGUP_FORM__,
 } from "./SingUpFormSettings";
 
 const SingUp = () => {
+	const [errorMessage, setErrorMessage] = useState("");
+
 	const handleSubmit = async (formData) => {
 		const { firstName, lastName, email, password } = formData;
 
 		try {
-			console.log(firstName, lastName, email, password);
+			setErrorMessage("");
 			// Add here the Fetch logic
-		} catch (err) {}
+		} catch (err) {
+			setErrorMessage(__ERROR_MESSAGE__);
+		}
 	};
 
 	return (
@@ -24,9 +30,26 @@ const SingUp = () => {
 			{({ isSubmitting }) => (
 				<Form>
 					<Field type="text" name="firstName" placeholder="Nombre" />
+					<ErrorMessage name="firstName">
+						{(error) => <div>{error}</div>}
+					</ErrorMessage>
+
 					<Field type="text" name="lastName" placeholder="Apellido" />
+					<ErrorMessage name="lastName">
+						{(error) => <div>{error}</div>}
+					</ErrorMessage>
+
 					<Field type="email" name="email" placeholder="Email" />
+					<ErrorMessage name="email">
+						{(error) => <div>{error}</div>}
+					</ErrorMessage>
+
 					<Field type="password" name="password" placeholder="Contraseña" />
+					<ErrorMessage name="password">
+						{(error) => <div>{error}</div>}
+					</ErrorMessage>
+
+					{errorMessage && <div>{errorMessage}</div>}
 
 					<button type="submit" disabled={isSubmitting}>
 						Enviar
--- src/components/SingUp/SingUpFormSettings.js
+++ src/components/SingUp/SingUpFormSettings.js
@@ -1,5 +1,16 @@
 import * as Yup from "yup";
 
+export const __ERROR_MESSAGE__ =
+	"Algo salió mal, por favor intenta de nuevo más tarde";
+const __ERROR_FIRSTNAME_REQUIRED__ = "¿Cómo te llamas?";
+const __ERROR_LASTNAME_REQUIRED__ = "Necesitamos saber tu apellido";
+const __ERROR_EMAIL_REQUIRED__ = "Escribe tu correo por favor";
+const __ERROR_EMAIL_INVALID__ = "El correo no es valido";
+const __ERROR_PASSWORD_REQUIRED__ =
+	"Necesitas una contraseña para poder ingresar";
+const __ERROR_PASSWORD_MIN_LENGTH__ =
+	"Tu contraseña debe tener al menos 6 caracteres";
+
 export const __INITIAL_VALUES_SINGUP_FORM__ = {
 	firstName: "",
 	lastName: "",
@@ -8,12 +19,12 @@ export const __INITIAL_VALUES_SINGUP_FORM__ = {
 };
 
 export const SingUpFormSchema = Yup.object().shape({
-	firstName: Yup.string().required("¿Cómo te llamas?"),
-	lastName: Yup.string().required("Necesitamos saber tu apellido"),
+	firstName: Yup.string().required(__ERROR_FIRSTNAME_REQUIRED__),
+	lastName: Yup.string().required(__ERROR_LASTNAME_REQUIRED__),
 	email: Yup.string()
-		.required("Escribe tu correo por favor")
-		.email("El correo no es valido"),
+		.required(__ERROR_EMAIL_REQUIRED__)
+		.email(__ERROR_EMAIL_INVALID__),
 	password: Yup.string()
-		.required("Necesitas una contraseña para poder ingresar")
-		.min(6, "Tu contraseña debe tener al menos 6 caracteres"),
+		.required(__ERROR_PASSWORD_REQUIRED__)
+		.min(6, __ERROR_PASSWORD_MIN_LENGTH__),
 });
